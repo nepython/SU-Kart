a1fd473af9a76bea303c0526f01b2cd212df5afc
Jain.Hardik <54471024+nepython@users.noreply.github.com>
2020-01-30 13:41:32 +0530

    Edited keys

--- SU_Kart/SU_Kart/settings.py
+++ SU_Kart/SU_Kart/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u9&m2c82ep8*^twp-!g1cm&bm=q4^9&8w5eb$2!yu#@g27ns)w'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hardikjain.pythonanywhere.com']
+ALLOWED_HOSTS = ['']
 
 # Application definition
 
